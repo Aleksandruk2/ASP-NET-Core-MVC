7bc273933ea0eef1734e18c509f21040d37dbbb4
Aleksandruk2 <aaleksandruk2018@gmail.com>
2025-12-24 04:22:06 +0200

    confirm add page- CreateCity

--- Classwork/WebApiTransfer/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Classwork/WebApiTransfer/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b045e9983d415516bd20a17d43dcaaec3caf62d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+210fa41122e8618993d8306d586e5a51671e5947")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
