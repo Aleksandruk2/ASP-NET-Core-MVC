87b50548bb323d7ced079d96d420d7c2a7f0140e
Aleksandruk2 <aaleksandruk2018@gmail.com>
2026-01-22 18:12:30 +0200

    added white theme

--- Classwork/my-transfer-ts/src/components/Account/Login/Login.tsx
+++ Classwork/my-transfer-ts/src/components/Account/Login/Login.tsx
@@ -46,7 +46,7 @@ const Login = () => {
                                     value={email}
                                     onChange={(e) => setEmail(e.target.value)}
                                     type="text"
-                                    className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                                    className="block w-full rounded-md dark:bg-white/5 bg-blue-200 px-3 py-1.5 dark:text-white text-gray-900 outline-1 -outline-offset-1 dark:outline-white/10 outline-blue-400 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
                                 />
                             </div>
                         </div>
